bd03dce8121282a0dc0108bdd2020e9fe0e72414
vetcovetimothy <timothy@vetcove.com>
2026-03-09 14:46:59 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
 module.exports = nextConfig;
